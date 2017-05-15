7d7646458d870a186588793fdf413fc448ead780
M.Rostami <M.Rostami>
2017-05-15 16:13:57 +0430

    ralavel-work with contorller and rout

---
+++ app/Http/Controllers/test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class test extends Controller
+{
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show()
+    {
+        return "lerning controller";
+    }
+}
